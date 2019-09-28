251526e853ad664420808e4f883252ff6723f2b2
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2019-09-29 00:08:24 +0200

    Moved addSchedule into AddScheduleCommand

--- apps/processing/src/commands/addScheduleCommand.ts
+++ apps/processing/src/commands/addScheduleCommand.ts
@@ -1,8 +1,10 @@
 import { GameRepository, SeasonInfoRepository, TeamRepository } from '@ffstats/database';
 import { Logger } from '@ffstats/logger';
+import { Game } from '@ffstats/models';
 import commandLineArgs from 'command-line-args';
+import fs from 'fs';
 import { Service } from 'typedi';
-import { addSchedule } from '../scheduleFunctions';
+import { Schedule } from '../models/schedule';
 import { ICommand } from './command.interface';
 
 @Service()
@@ -41,12 +43,75 @@ export class AddScheduleCommand implements ICommand {
 
   public async run(): Promise<void> {
     for (const file of this.files) {
-      await addSchedule(
-        file,
-        this.teamRepository,
-        this.gameRepository,
-        this.seasonInfoRepository
-      );
+      await this.addSchedule(file);
+    }
+  }
+
+  private async addSchedule(scheduleFile: string, force: boolean = false): Promise<void> {
+    const schedule = JSON.parse(fs.readFileSync(scheduleFile, 'utf-8')) as Schedule;
+
+    let weeksInDb = await this.gameRepository.getWeeksInYear(schedule.year);
+
+    if (weeksInDb.length > 0 && force) {
+      // TODO: delete games in db
+      weeksInDb = [];
+    }
+
+    const teams = new Map(
+      (await this.teamRepository.get()).map(team => [team.owner, team])
+    );
+    const gamesToAdd: Pick<Game, 'year' | 'week' | 'gameScores'>[] = [];
+
+    schedule.weeks.forEach(week => {
+      if (weeksInDb.includes(week.week)) {
+        // this week already exists in database
+        return;
+      }
+
+      week.games.forEach(game => {
+        if (!teams.has(game.team1)) {
+          throw new Error(`Cannot find team ${game.team1}`);
+        }
+        if (!teams.has(game.team2)) {
+          throw new Error(`Cannot find team ${game.team2}`);
+        }
+
+        const team1 = teams.get(game.team1);
+        const team2 = teams.get(game.team2);
+
+        gamesToAdd.push({
+          year: schedule.year,
+          week: week.week,
+          gameScores: [
+            {
+              id: 0, // NOTE: should not be needed
+              year: schedule.year,
+              week: week.week,
+              team_id: team1.id,
+              game_id: 0 // NOTE: should not be needed
+            },
+            {
+              id: 0, // NOTE: should not be needed
+              year: schedule.year,
+              week: week.week,
+              team_id: team2.id,
+              game_id: 0 // NOTE: should not be needed
+            }
+          ]
+        });
+      });
+    });
+
+    await this.gameRepository.createWithGameScores(gamesToAdd);
+
+    if (!(await this.seasonInfoRepository.get('year', schedule.year, true))) {
+      await this.seasonInfoRepository.create({
+        year: schedule.year,
+        num_teams: schedule.numTeams,
+        num_playoff_teams: schedule.numPlayoffTeams,
+        regular_season_length: schedule.regularSeasonLength,
+        playoff_length: schedule.playoffLength
+      });
     }
   }
 }
